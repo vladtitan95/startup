7f6ff56401ac750d72ac6b9ad4c50dd278b08df6
Your Name <you@example.com>
2019-10-31 20:54:07 +0200

    Сделал еще один блок, настроил второй слайдер

--- script/slider.js
+++ script/slider.js
@@ -7,5 +7,14 @@ $(document).ready(function(){
         prevArrow:'<button type="button" class="slick-prev"><img src="/src/img/prev.png"></button>',
         nextArrow:'<button type="button" class="slick-next"><img src="/src/img/next.png"></button>'
     });
+    $('.transform__slider').slick({
+        dots:true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow:'<button type="button" class="slick-prev-new"><img class="slick-prev-img" src="/src/img/prev.png"></button>',
+        nextArrow:'<button type="button" class="slick-next-new"><img class="slick-next-img" src="/src/img/next.png"></button>'
+    });
 
 });
